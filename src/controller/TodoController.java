ff70d21f3e0e1f2541bebe51bd8c12a8506531f0
aasiskrk <aasis.krk1@gmail.com>
2022-11-26 07:33:21 +0545

    todo task completed

---
+++ src/controller/TodoController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import database.DbConnection;
+import model.Todo;
+
+/**
+ *
+ * @author aasis
+ */
+public class TodoController {
+    DbConnection dbConnection;
+
+    public int insertTodo(Todo todo){
+        String myTodo = todo.getText();
+        String insertQuery = String.format(
+      "INSERT INTO todo_data VALUES('%s')",
+      myTodo
+    );
+        dbConnection = new DbConnection();
+
+    int result = dbConnection.manipulate(insertQuery);
+    return result;
+    }
+}
--- src/view/Listtodo.java
+++ src/view/Listtodo.java
@@ -4,6 +4,11 @@
  */
 package view;
 
+import controller.NotesController;
+import model.Notes;
+import controller.TodoController;
+import model.Todo;
+
 /**
  *
  * @author ASUS
@@ -32,7 +37,7 @@ public class Listtodo extends javax.swing.JFrame {
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
         jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
+        Todo = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -51,13 +56,18 @@ public class Listtodo extends javax.swing.JFrame {
 
         jTextField5.setText("Task 5");
 
-        jTextField6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jTextField6.setText("Add To do");
+        Todo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        Todo.setText("Add To do");
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("TODO List");
 
         jButton1.setText("ADD");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton2.setText("Delete");
@@ -76,7 +86,7 @@ public class Listtodo extends javax.swing.JFrame {
                             .addComponent(jTextField3)
                             .addComponent(jTextField4)
                             .addComponent(jTextField5, javax.swing.GroupLayout.DEFAULT_SIZE, 415, Short.MAX_VALUE)
-                            .addComponent(jTextField6))
+                            .addComponent(Todo))
                         .addGap(18, 18, 18)
                         .addComponent(jButton1))
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -93,7 +103,7 @@ public class Listtodo extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Todo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addGap(45, 45, 45)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -123,6 +133,14 @@ public class Listtodo extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+                String text=Todo.getText();
+Todo newTodo = new Todo(text);        
+TodoController nc = new TodoController();
+int insertedTodo = nc.insertTodo(newTodo);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -160,6 +178,7 @@ public class Listtodo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Todo;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
@@ -169,6 +188,5 @@ public class Listtodo extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
     // End of variables declaration//GEN-END:variables
 }
