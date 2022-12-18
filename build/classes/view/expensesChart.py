5365e16bfed92d1e3c26e977c8b1ea21557add99
PrasannaJung <prasannajung08@gmail.com>
2022-12-18 13:47:43 +0545

    Pie chart for expenses made

---
+++ build/classes/view/expensesChart.py
@@ -0,0 +1,48 @@
+# import mysql connector
+import mysql.connector
+# to make a pie chart from mysql table named expenses which has 3 columns named  name , cost and date
+
+# connect to mysql database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root",
+  database="apex"
+)
+
+# create a cursor
+mycursor = mydb.cursor()
+
+# execute the query
+mycursor.execute("SELECT expense_name, expense_amount FROM expenses_data")
+
+# fetch all the records
+result = mycursor.fetchall()
+
+# create a list of tuples
+
+# create a list of tuples
+expenses = []
+for row in result:
+    expenses.append((row[0],row[1]))
+
+# create a pie chart
+import matplotlib.pyplot as plt
+
+# create a list of colors
+colors = ["red","green","blue","yellow","orange","purple","brown","pink","grey","cyan"]
+
+# create a pie chart
+
+plt.pie([row[1] for row in expenses], labels=[row[0] for row in expenses], colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+
+# add a title
+plt.title("Expenses")
+# save the plot with dimensions in pixels of 100x100
+
+plt.savefig("py_expenses.png", bbox_inches="tight", pad_inches=0.2, dpi=100)
+
+
+# show the plot
+plt.show()
+
--- src/view/ExpenseForm.java
+++ src/view/ExpenseForm.java
@@ -6,6 +6,7 @@ package view;
 import java.sql.*;
 
 import controller.ExpenseController;
+import java.io.IOException;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 import model.ExpenseModel;
@@ -22,6 +23,17 @@ public class ExpenseForm extends javax.swing.JFrame {
   public ExpenseForm() {
     initComponents();
     table_update();
+     try {
+      // Execute the Python script
+      Process process = Runtime.getRuntime().exec("python expensesChart.py");
+      
+
+      // Wait for the script to finish executing
+      process.waitFor();
+      System.out.println("Python script ran");
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
@@ -45,6 +57,7 @@ public class ExpenseForm extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -169,14 +182,27 @@ public class ExpenseForm extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setBackground(new java.awt.Color(0, 102, 0));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Expenses Chart");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(61, 61, 61)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 578, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -189,8 +215,10 @@ public class ExpenseForm extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
-                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 136, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28))
         );
 
@@ -240,11 +268,14 @@ public class ExpenseForm extends javax.swing.JFrame {
           }
           
           Df.addRow(v2);
+          
       }
       }
       catch(SQLException e){
           e.printStackTrace();
       }
+      
+      
 
   }
   
@@ -274,6 +305,12 @@ public class ExpenseForm extends javax.swing.JFrame {
         al.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        ExpenseChart eChart = new ExpenseChart();
+        eChart.setVisible(true);
+        this.dispose();        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
   /**
    * @param args the command line arguments
@@ -325,6 +362,7 @@ public class ExpenseForm extends javax.swing.JFrame {
     private javax.swing.JTextField date;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
